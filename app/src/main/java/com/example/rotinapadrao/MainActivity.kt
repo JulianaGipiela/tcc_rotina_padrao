42de38bd2ed22fbb99a4bbef5af44f6a055f12c6
Juliana Cecilia Gipiela Correa Dias <jdias@rpc.com.br>
2024-05-04 09:45:44 -0300

    incluido cabeçalho versao 1

--- app/src/main/java/com/example/rotinapadrao/MainActivity.kt
+++ app/src/main/java/com/example/rotinapadrao/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.rotinapadrao
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rotinapadrao.databinding.ActivityMainBinding
+import com.example.rotinapadrao.databinding.HeaderLayoutBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var headerBinding: HeaderLayoutBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        headerBinding = HeaderLayoutBinding.bind(binding.root.findViewById(R.id.headerLayout))
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        // Configuração do clique do botão de logout
+        headerBinding.logoutButton.setOnClickListener {
+            // Realiza o logout do usuário
+            auth.signOut()
+
+            // Após fazer logout, redirecione o usuário para a tela de login
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish() // Fecha a MainActivity após iniciar a LoginActivity
+        }
     }
 }
