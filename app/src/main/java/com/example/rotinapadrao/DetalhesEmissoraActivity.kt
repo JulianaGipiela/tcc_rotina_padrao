bd52c6279c1c527cc1fcffb274937c9c0e052cdf
Juliana Cecilia Gipiela Correa Dias <jdias@rpc.com.br>
2024-05-06 08:05:42 -0300

    banco de emissoras

--- app/src/main/java/com/example/rotinapadrao/DetalhesEmissoraActivity.kt
+++ app/src/main/java/com/example/rotinapadrao/DetalhesEmissoraActivity.kt
@@ -3,29 +3,40 @@ package com.example.rotinapadrao
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class DetalhesEmissoraActivity : AppCompatActivity() {
+
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes_emissora)
 
+        // Inicialize o Firebase
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+
         val idEmissora = intent.getIntExtra("ID_EMISSORA", -1)
-        val nomeEmissora = getNomeEmissoraFromId(idEmissora)
-        supportActionBar?.title = nomeEmissora
+        val emissoraRef = database.getReference("emissora").child(idEmissora.toString())
 
+        // Adicione um ouvinte de valor para recuperar o nome da emissora do Firebase Realtime Database
+        emissoraRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val nomeEmissora = snapshot.getValue(String::class.java)
+                if (nomeEmissora != null) {
+                    supportActionBar?.title = nomeEmissora
 
-        val textViewTituloEmissora = findViewById<TextView>(R.id.textViewTituloEmissora)
-        textViewTituloEmissora.text = nomeEmissora
-    }
+                    val textViewTituloEmissora = findViewById<TextView>(R.id.txtEmissoraTitle)
+                    textViewTituloEmissora.text = nomeEmissora
+                }
+            }
 
-    private fun getNomeEmissoraFromId(idEmissora: Int): String {
-        return when (idEmissora) {
-            1 -> "Emissora 1"
-            2 -> "Emissora 2"
-            3 -> "Emissora 3"
-            4 -> "Emissora 4"
-            else -> "Emissora Desconhecida"
-        }
+            override fun onCancelled(error: DatabaseError) {
+                // Trate o erro, se necessário
+            }
+        })
     }
 }
