6349b654dfd4ee8279793abd6bc18cb8bfb00fb6
Juliana Cecilia Gipiela Correa Dias <jdias@rpc.com.br>
2024-05-06 09:33:50 -0300

    incluido os sites na tela

--- app/src/main/java/com/example/rotinapadrao/DetalhesEmissoraActivity.kt
+++ app/src/main/java/com/example/rotinapadrao/DetalhesEmissoraActivity.kt
@@ -1,12 +1,14 @@
 package com.example.rotinapadrao
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+
 class DetalhesEmissoraActivity : AppCompatActivity() {
 
     private lateinit var database: FirebaseDatabase
@@ -21,16 +23,34 @@ class DetalhesEmissoraActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         val idEmissora = intent.getIntExtra("ID_EMISSORA", -1)
-        val emissoraRef = database.getReference("emissora").child(idEmissora.toString()).child("nome")
+        val emissoraRef = database.getReference("emissora").child(idEmissora.toString())
 
         // Adicione um ouvinte de valor para recuperar os dados da emissora do Firebase Realtime Database
         emissoraRef.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val nomeEmissora = snapshot.getValue(String::class.java)
-                if (nomeEmissora != null) {
-                    supportActionBar?.title = nomeEmissora
+                val emissora = snapshot.child("nome").getValue(String::class.java)
+                if (emissora != null) {
+                    supportActionBar?.title = emissora
+
                     val textViewTituloEmissora = findViewById<TextView>(R.id.txtEmissoraTitle)
-                    textViewTituloEmissora.text = nomeEmissora
+                    textViewTituloEmissora.text = emissora
+
+                    val sitesSnapshot = snapshot.child("sites")
+                    val layoutBotoesSites = findViewById<LinearLayout>(R.id.layoutBotoesSites)
+                    layoutBotoesSites.removeAllViews()
+
+                    for (siteSnapshot in sitesSnapshot.children) {
+                        val siteId = siteSnapshot.key
+                        val siteNome = siteSnapshot.getValue(String::class.java)
+                        if (siteNome != null && siteId != null) {
+                            val button = Button(this@DetalhesEmissoraActivity)
+                            button.text = siteNome
+                            button.setOnClickListener {
+                                // Implemente a ação do botão aqui, se necessário
+                            }
+                            layoutBotoesSites.addView(button)
+                        }
+                    }
                 }
             }
 
@@ -39,6 +59,4 @@ class DetalhesEmissoraActivity : AppCompatActivity() {
             }
         })
     }
-
-
 }
--- app/src/main/java/com/example/rotinapadrao/Emissora.kt
+++ app/src/main/java/com/example/rotinapadrao/Emissora.kt
@@ -2,5 +2,5 @@ package com.example.rotinapadrao
 
 data class Emissora(
     val nome: String = "",
-    val site: Map<String, String> = emptyMap()
+    val sites: Map<String, String> = emptyMap()
 )
