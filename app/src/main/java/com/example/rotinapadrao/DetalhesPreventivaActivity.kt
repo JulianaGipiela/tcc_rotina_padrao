921f1b97935f12208d158e2cd0418ad71f0485d4
Juliana Cecilia Gipiela Correa Dias <jdias@rpc.com.br>
2024-05-07 09:24:11 -0300

    melhorando a visualização dos dados

--- app/src/main/java/com/example/rotinapadrao/DetalhesPreventivaActivity.kt
+++ app/src/main/java/com/example/rotinapadrao/DetalhesPreventivaActivity.kt
@@ -1,6 +1,8 @@
 package com.example.rotinapadrao
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,13 +14,15 @@ class DetalhesPreventivaActivity : AppCompatActivity() {
         val txtData = findViewById<TextView>(R.id.txtData)
         val txtResponsavel = findViewById<TextView>(R.id.txtResponsavel)
         val txtDescricao = findViewById<TextView>(R.id.txtDescricao)
+        val checkboxRealizado = findViewById<CheckBox>(R.id.inspecaoCheck)
 
         val nomeSite = intent.getStringExtra("NOME_SITE")
         val nomeEmissora = intent.getStringExtra("NOME_EMISSORA")
         val data = intent.getStringExtra("DATA")
         val responsavel = intent.getStringExtra("RESPONSAVEL")
         val descricao = intent.getStringExtra("DESCRICAO")
-
+        val isRealizado = intent.getBooleanExtra("REALIZADO", false)
+        Log.d("DetalhesPreventivaActivity", "Valor recuperado do Firebase: $isRealizado")
         val txtNomeSite = findViewById<TextView>(R.id.txtNomeSite)
         val txtNomeEmissora = findViewById<TextView>(R.id.txtNomeEmissora)
 
@@ -27,5 +31,8 @@ class DetalhesPreventivaActivity : AppCompatActivity() {
         txtData.text = "Data: $data"
         txtResponsavel.text = "Responsável: $responsavel"
         txtDescricao.text = "Descrição: $descricao"
+
+        checkboxRealizado.isChecked = isRealizado
+        checkboxRealizado.isEnabled = false
     }
 }
